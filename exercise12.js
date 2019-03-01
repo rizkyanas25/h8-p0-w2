3488057b69345db2a8de1d2950ac8a23a17fde2c
rizkyanas35 <rizkyanas25@gmail.com>
2019-03-01 20:41:19 +0700

    new assignment

---
+++ exercise12.js
@@ -0,0 +1,21 @@
+function konversiMenit(menit) {
+    // you can only write your code here!
+    var text = ''
+    var hour = Math.floor (menit / 60)
+    var minute = menit % 60
+        if(minute < 10) {
+            text = hour + ':0' + minute
+        } else if(minute >= 10 && minute <= 59) {
+            text = hour + ':' + minute
+        }
+        
+       
+    return text
+  }
+  
+  // TEST CASES
+  console.log(konversiMenit(63)); // 1:03
+  console.log(konversiMenit(124)); // 2:04
+  console.log(konversiMenit(53)); // 0:53
+  console.log(konversiMenit(88)); // 1:28
+  console.log(konversiMenit(120)); // 2:00
---
+++ exercise13.js
@@ -0,0 +1,20 @@
+function xo(str) {
+    // you can only write your code here!
+    var o = 0;
+    var x = 0;
+    for(var i =0; i<str.length; i++){
+      if(str[i]==='o'){
+        o++;
+      }else{
+        x++;
+      }
+    }
+    return (o===x);
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
